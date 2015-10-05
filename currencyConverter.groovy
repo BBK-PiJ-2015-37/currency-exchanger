42d7d312dbb67526371c4ece2eaaa35cd72c99bb
unknown <sjanse01@CSPC050.dcs.bbk.ac.uk>
2015-10-05 19:23:26 +0100

    Tweaking output, adding calculation messages

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -1,8 +1,8 @@
-print "If I give you £1, how many Euro will you give me? ";
+﻿print "If I give you £1, how many Euro will you give me? ";
 double euroOverPoundRatio = Double.parseDouble(System.console().readLine());
 print "If I give you 1€, how many Sterling pounds will you give me? ";
 double poundOverEuroRatio = Double.parseDouble(System.console().readLine());
-println "OK, now I know enough.";
+println "Sufficient information to calculate exchange rates!";
 boolean finished = false;
 while (!finished) {
     println "";
@@ -17,12 +17,14 @@ while (!finished) {
     case 1:
 	  print "How many pounds would you like to convert? ";
 	  double pounds = Double.parseDouble(System.console().readLine());
+          println "Calculating..."
 	  double euro   = pounds * euroOverPoundRatio;
 	  println "£" + pounds + " will give you " + euro + "€";
 	  break;
     case 2: 
 	  print "How many euro would you like to convert? ";
 	  double euro   = Double.parseDouble(System.console().readLine());
+          println "Calculating..."
 	  double pounds = euro * poundOverEuroRatio;
 	  println euro + "€ will give you £" + pounds;
 	  break;
